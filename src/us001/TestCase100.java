2c9cc47af445a3bc9543c704bd86c603afe760df
Koray <basoglukoray1@gmail.com>
2022-01-11 12:13:13 -0500

    main and add comment

--- src/us001/TestCase100.java
+++ src/us001/TestCase100.java
@@ -2,7 +2,7 @@ package us001;
 
 public class TestCase100 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello"); // commen
     }
 
 }
